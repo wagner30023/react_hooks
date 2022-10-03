639de96050554ff61029efc80ac54c3dc2aaf9a2
Carlos Wagner <wagner30023@gmail.com>
2022-10-03 11:48:38 -0300

    useReducer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import HookUseState from '../components/HookUseState'
-
+import HookUseReducer from '../components/HookUseReducer'
 const Home = () => {
   return (
     <div>
       <HookUseState />
+      <HookUseReducer />
     </div>
   )
 }
